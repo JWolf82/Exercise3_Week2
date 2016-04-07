e628f2f32d7173f2d38a9bedd731fdb49f511ae6
JWolf82 <jlwolf82@gmail.com>
2016-04-07 23:07:04 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// var phone = prompt("What is your phone number? XXX-XXX-XXXX");
+
+// if(phone.charAt(3) === "-" && phone.charAt(7) === "-"){
+//     alert("Your phone number is " + phone);
+// } else {
+//     alert("This is not a valid number.");
+// }
+
+// var birthDate = prompt("What is your birth date? XX/XX/XXXX");
+
+//     if(birthDate.charAt(2) === "/" && birthDate.charAt(5) === "/"){
+//         alert("Your birthday is " + birthDate);
+//     } else {
+//         alert("Invalid Birthday");
+//     }
+
+// var postal = prompt("What is your zipcode");
+
+// if(postal.length === 5 && isFinite(postal)){
+//     alert("Your zip is " + postal);
+// } else {
+//     alert("Invalid");
+// }
+
+// var state = prompt("What is your state?");
+
+// if(state.length === 2 && state === state.toUpperCase() && isNaN(state)){
+//     alert("Your state is " + state);
+// } else {
+//     alert("Invalid");
+// }
+
+// var married = prompt("Are you Married?");
+
+// if(married === married.toUpperCase || married === married.toLowerCase || married === "Yes"){
+//         alert("You are married.")
+// } else {
+//     alert("You are not married.")
+// }
